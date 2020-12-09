5f1c70d37ac1349d5cf414d22f56ab65a50b141e
Filip Bartosiak <filipbart1@gmail.com>
2020-12-09 11:17:50 +0100

    Fix widget

--- lib/widgets/adaptive_flat_button.dart
+++ lib/widgets/adaptive_flat_button.dart
@@ -7,29 +7,29 @@ class AdaptiveFlatButton extends StatelessWidget {
   final String text;
   final Function handler;
 
-  AdaptiveFlatButton(this.text, this.handler)
+  AdaptiveFlatButton(this.text, this.handler);
 
   @override
   Widget build(BuildContext context) {
     return Platform.isIOS
-                        ? CupertinoButton(
-                            onPressed: handler,
-                            child: Text(
-                              text,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          )
-                        : FlatButton(
-                            textColor: Theme.of(context).primaryColor,
-                            onPressed: handler,
-                            child: Text(
-                              text,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          )
+        ? CupertinoButton(
+            onPressed: handler,
+            child: Text(
+              text,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        : FlatButton(
+            textColor: Theme.of(context).primaryColor,
+            onPressed: handler,
+            child: Text(
+              text,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          );
   }
 }
